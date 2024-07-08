6dbefb14276e58165b64b9987f6eed9826ba22e2
Oleg Sirenko <spec_oleg@mail.ru>
2024-07-09 01:43:25 +0300

    Updated for checking chat discussion

--- resources/config.hpp
+++ resources/config.hpp
@@ -8,6 +8,7 @@ namespace Config {
     namespace chat_ids {
         const long main_channel_id = -1001772576895;
         const long dev_channel_id = -1001812728287;
+        const long main_discussion_id = -1001810809387;
     }
     namespace admin_ids {
         const int tehnokratgod = 540929323;
